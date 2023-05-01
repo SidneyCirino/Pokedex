f7c7329a3cd450d11dfdb88d72aa2c11f39c75e1
Sidney <sidney.nutri@gmail.com>
2023-05-01 20:57:53 -0300

    inicia interação com pokeAPI e abstrai codigos do js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,30 @@
-const btnTeste = document.getElementById("btnTeste");
-btnTeste.addEventListener("click", ()=> {
-    console.log('Clickou!')
-})
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="numero">#001</span>
+            <span class="nome">${pokemon.name}</span>
+
+            <div class="detalhes">
+                <ol class="tipos">
+                    <li class="tipo">Grama</li>
+                    <li class="tipo">Veneno</li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons) => {
+        for (let i = 1; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        }
+    })
